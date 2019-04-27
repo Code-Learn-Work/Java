995808dbb82217f4f4413ec10c42966575927083
Mayank Khanna <khanna98@outlook.com>
2019-04-27 23:02:42 +0530

    Singly Linked List

---
+++ 04-Linked List/Node.java
@@ -0,0 +1,11 @@
+package LinkedList;
+
+public class Node<T>{
+	T data;
+	Node<T> next;
+	public Node(T data) {
+		// TODO Auto-generated constructor stub
+		this.data = data;
+		next = null;
+	}
+}
